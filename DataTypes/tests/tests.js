68c0e84c96fc5c16607ab8909f9e759552658f5d
eyo.omat@gmail.com <eyo.omat@gmail.com>
2017-01-08 02:14:07 +0100

    added correct test cases from andelabs

--- DataTypes/tests/tests.js
+++ DataTypes/tests/tests.js
@@ -5,63 +5,93 @@ var myApp = require('../app/library.js');
 var saitama;
 var junk = null;
 (function() { 
-  describe('Data types of a certain variable: ', function () {
+  describe("Data types tests ", function() {
+  describe("Case for some falsy values", function() {
 
-    describe('Return the length of the the string', function () {
-
-      it('should return 4 for `gate`', function () {
-        expect(myApp.dataType("gate")).toEqual(4);
-      });
-
-      it('should return 2 for `to`', function () {
-        expect(myApp.dataType("to")).toEqual(2);
-      });
-
-      it('should return 12 for `hippopotamus`', function () {
-        expect(myApp.dataType("hippopotamus")).toEqual(12);
-      });
-
-      it('should return 10 for `unchatered`', function () {
-        expect(myApp.dataType("unchatered")).toEqual(10);
-      });
-
+    it("should return 'no value' for null", function() {
+      expect(myApp.dataTypes(null)).toEqual('no value');
     });
 
-    describe('Return `no value` for an undefined variable', function () {
-
-      it('should return "no value" for saitama', function () {
-        expect(myApp.dataType(saitama)).toEqual("no value");
-      });
-
-      it('should return "no value" for undefined', function () {
-        expect(myApp.dataType(undefined)).toEqual("no value");
-      });
-
-    });
-
-    describe('Return boolean value for an boolean variable', function () {
-
-      it('should return true for true', function () {
-        expect(myApp.dataType(true)).toEqual(true);
-      });
-
-      it('should return false for false', function () {
-        expect(myApp.dataType(false)).toEqual(false);
-      });
-
-    });
-
-    describe('Return `no value` for an null variable', function () {
-
-      it('should return "no value" for null', function () {
-        expect(myApp.dataType(null)).toEqual("no value");
-      });
-
-      it('should return "no value" for junk', function () {
-        expect(myApp.dataType(junk)).toEqual("no value");
-      });
-
+    it("should return 'no value' for undefined", function() {
+      expect(myApp.dataTypes(undefined)).toEqual('no value');
     });
 
   });
+
+  describe("Case for booleans", function() {
+
+    it("should return true for `true`", function() {
+      expect(myApp.dataTypes(true)).toBe(true);
+    });
+
+    it("should return false for `false`", function() {
+      expect(myApp.dataTypes(false)).toBe(false);
+    });
+
+  });
+
+  describe("Case for Numbers", function() {
+
+    it("should return 'less than 100' for 44", function() {
+      expect(myApp.dataTypes(44)).toBe('less than 100');
+    });
+
+    it("should return 'more than 100' for 144", function() {
+      expect(myApp.dataTypes(144)).toBe('more than 100');
+    });
+
+    it("should return 'equal to 100' for 100", function() {
+      expect(myApp.dataTypes(100)).toBe('equal to 100');
+    });
+
+
+  });
+
+  describe("Case for Strings", function() {
+
+    it("should return the length of `tergiversate`", function() {
+      expect(myApp.dataTypes('tergiversate')).toBe(12);
+    });
+
+    it("should return the length of an empty string", function() {
+      expect(myApp.dataTypes('')).toBe(0);
+    });
+
+    it("should return the length of `555`", function() {
+      expect(myApp.dataTypes('555')).toBe(3);
+    });
+
+  });
+
+  describe("Case for arrays", function() {
+
+    it("should return `2` for `[0, 1, 2]`", function() {
+      expect(myApp.dataTypes([0, 1, 2])).toBe(2);
+    });
+
+    it("should return `undefined` for `[]`", function() {
+      expect(myApp.dataTypes([])).not.toBeDefined();
+    });
+
+    it("should return `undefined` for `[4, 9]`", function() {
+      expect(myApp.dataTypes([4, 9])).not.toBeDefined();
+    });
+
+  });
+
+
+  describe("Case for functions", function() {
+
+    it("should call the `callback` function with argument true, and return `called callback`", function() {
+      var callback = function(arg) {
+        expect(arg).toBeTruthy();
+        if(arg === true) {
+          return 'called callback';
+        }
+      };
+      expect(myApp.dataTypes(callback)).toBe('called callback');
+    });
+
+  });
+});
 })();
