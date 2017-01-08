f6c4cc6ed52ea1f8157faf520bb8940e6a621547
eyo.omat@gmail.com <eyo.omat@gmail.com>
2017-01-08 01:03:15 +0100

    created functions to check data types

--- DataTypes/app/library.js
+++ DataTypes/app/library.js
@@ -12,11 +12,24 @@ dataType: function(variable){
     if (typeof variable === 'boolean') {
         return variable;
     }
-    if (typeof variable === 'string') {
-        return variable.length;
+    if (typeof variable === 'number') {
+        if (variable < 100) {
+            return 'less than 100';
+        } else if (variable > 100) {
+            return 'more than 100';
+        } else {
+            return 'equal to 100';
+        }
     }
-    if (typeof variable === 'string') {
-        return variable.length;
+    if (typeof variable.isArray()) {
+        if (variable,lenght < 4) {
+            return 'undefined';
+        } else {
+            return variable[3];
+        }        
+    }
+    if (typeof variable === 'function') {
+        return variable(true);
     }
         
 }
